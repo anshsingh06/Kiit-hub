b9011826403f9daec1f5056892e97c8e28347e39
anshsingh06 <anshsingh01227@gmail.com>
2025-06-12 23:31:40 +0530

    comment db

---
+++ server/models/comment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
+
+
+
