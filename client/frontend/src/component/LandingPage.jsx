ec3da1846ca91404ddfd36f1d2d4b4bd0c5084e6
anshsingh06 <anshsingh01227@gmail.com>
2025-06-27 23:58:43 +0530

    sign up white added

--- client/frontend/src/component/LandingPage.jsx
+++ client/frontend/src/component/LandingPage.jsx
@@ -99,7 +99,7 @@ function LandingPage() {
     </button>
 
     {showSignUp && (
-      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-white bg-opacity-50 flex items-center justify-center z-50">
         <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6 relative">
           <button
             className="absolute top-2 right-2 text-gray-600 hover:text-black text-xl"
