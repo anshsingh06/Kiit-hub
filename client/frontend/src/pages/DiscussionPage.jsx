60931443116e8d7cb62cbb63e96643c200110179
anshsingh06 <anshsingh01227@gmail.com>
2025-07-06 23:54:33 +0530

    bg color fixed

--- client/frontend/src/pages/DiscussionPage.jsx
+++ client/frontend/src/pages/DiscussionPage.jsx
@@ -122,7 +122,7 @@ function DiscussionPage() {
               />
               <button
                 onClick={() => handleAddAnswer(post._id)}
-                className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600"
+                className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-green-600"
               >
                 Comment
               </button>
