fdb46a2016deecd872bc77ee1084807addb502c8
anshsingh06 <anshsingh01227@gmail.com>
2025-07-22 01:19:37 +0530

    fixing issue

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const postRoutes = require('./routes/postroutes');
 const commentRoutes = require('./routes/commentRoutes');
 const newsRoutes = require('./routes/newsRoutes');
 const cors = require('cors');
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/posts', postRoutes);
